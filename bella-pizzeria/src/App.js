99498c0bcd19223b73a337a79fa907d041553b3f
Nikolai <cooprix1@gmail.com>
2021-11-19 15:00:48 +0100

    Added ThemeProvider

--- bella-pizzeria/src/App.js
+++ bella-pizzeria/src/App.js
@@ -2,24 +2,24 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const theme = createTheme({
+    palette: {
+      primary: teal,
+      secondary: amber
+    }
+  });
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        
+      </div>
+    </ThemeProvider>
   );
 }
+
+export default App;
 
 export default App;
