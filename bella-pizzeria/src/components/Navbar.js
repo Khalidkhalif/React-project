ec2b0e294663b9de8434e6f7b053ccf98777b1e4
Nikolai <cooprix1@gmail.com>
2021-11-23 18:10:33 +0100

    Added Navbar

--- bella-pizzeria/src/components/Navbar.js
+++ bella-pizzeria/src/components/Navbar.js
@@ -1,4 +1,4 @@
-import React from 'react';
+/*import React from 'react';
 import { Link } from 'react-router-dom'
  const Navbar = ()=>{
     return(
@@ -16,4 +16,58 @@ import { Link } from 'react-router-dom'
     )
 }
 
-export default Navbar;
+export default Navbar;*/
+
+import React from "react";
+
+import {Toolbar, Typography} from "@material-ui/core";
+import {makeStyles} from "@material-ui/core/styles";
+import { Link } from "@mui/material";
+
+
+
+const styles = makeStyles({
+    bar:{
+        paddingTop:"1.15rem",
+        backgroundColor:"black",
+        ['@media (max-width:780px)']:{
+            flexDirection:"column"
+        }
+    },
+    menuItem:{
+        cursor:"pointer",
+        flexGrow:1,
+        "&hover":{
+            color:"#4f25c8"
+        },
+        ['@media(max-width:780px']:{
+            paddingBottom:"1rem"
+        }
+    }
+})
+function NavBar() {
+    const classes = styles()
+    return (
+        <nav className="nav-wrapper">
+        <Toolbar position="sticky"  className={classes.bar}>
+            <Link to="/pages/Home"><Typography style={{color:"white"}} variant="h6" className={classes.menuItem}>
+            Home
+            </Typography></Link>
+            <Typography style={{color:"white"}} variant="h6" className={classes.menuItem}>
+                Menu
+            </Typography>
+            <Typography style={{color:"white"}} variant="h6" className={classes.menuItem}>
+                Check out
+            </Typography>
+            <Typography style={{color:"white"}} variant="h6" className={classes.menuItem}>
+                Contact us
+            </Typography>
+            
+        </Toolbar>
+        </nav>
+
+
+    )
+}
+
+export default NavBar;
